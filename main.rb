97cab6e0b60eac10e7533ec3443194ee376d701e
astux7 <Asta.bevainyte@gmail.com>
2013-12-11 13:02:37 +0000

    typos and refactoring

--- main.rb
+++ main.rb
@@ -1,18 +1,25 @@
 require_relative './lib/cracklepop'
-
+#output data to file
 def print_to_file
   File.open('first100.txt', "w") do |file|
-    @result.each do |element|
-      file.puts element
-    end
+    @result.each {|element| file.puts element }
   end
 end
-
+#creating array of result
+def make_cracklepop
 cp = CracklePop.new
-@result = []
 1.upto(100) {|number|
   @result << cp.cracklepop(number)
 }
+end
+#printin result to console
+def print_to_console
+  @result.each {|element| puts element}
+end
+#======== MAIN ==============
+@result = []
+make_cracklepop
 print_to_file
+print_to_console
 
  
